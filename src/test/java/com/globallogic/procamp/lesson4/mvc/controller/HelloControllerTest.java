4e09dfebe79540a02a3d8e9557a95332bb697beb
Andriy Sivachok <andriysivachok@ziftsolutions.com>
2018-10-30 10:13:02 +0200

    lesson 4 rest mvc hello view

---
+++ src/test/java/com/globallogic/procamp/lesson4/mvc/controller/HelloControllerTest.java
@@ -0,0 +1,18 @@
+package com.globallogic.procamp.lesson4.mvc.controller;
+
+import static org.junit.Assert.*;
+import static org.springframework.core.Conventions.getVariableName;
+
+import org.junit.Test;
+
+public class HelloControllerTest {
+
+    @Test
+    public void displayList() {
+
+        String andriiSivachok = "Andrii Sivachok";
+        Integer i = 100;
+        
+        System.out.println(getVariableName(i));
+    }
+}
